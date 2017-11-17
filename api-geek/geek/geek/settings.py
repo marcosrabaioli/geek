4c084cb1839b194273dbf6ec30a49a09d1b87b8a
Marcos Rabaioli <marcos.rabaioli@chipinside.com.br>
2017-11-17 17:26:05 -0200

    Mudado database

--- api-geek/geek/geek/settings.py
+++ api-geek/geek/geek/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'geek.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'geekdb',
         'USER': 'geeknuser',
         'PASSWORD': 'geekpass',
